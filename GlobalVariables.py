d5bc59e8a167399a928ba2c1cfb76a403f1db383
Christian Boin <christianboin@Christians-MacBook-Pro-2.local>
2020-07-11 19:59:13 -0400

    Added sep branches for new versions             -- Modified program to use mp3 tagged song name for file name             -- Modified program to keep track of whether there is an album artist

--- GlobalVariables.py
+++ GlobalVariables.py
@@ -1,3 +1,6 @@
+VERSION_NUMBER = '2.2'
+
+
 PLAYING_STRING_DEFAULT = "Playing: %s - %s: %s."
 PLAYING_STRING_COMMANDS_DEFAULT = "\nCommands:\n - q (stop),\n - space (pause/resume), \n - d (next),\n - a (restart),\n - z (previous),\n - 0-9 (set volume)"
 PLAYING_STRING_COMMANDS_SPECIAL = PLAYING_STRING_COMMANDS_DEFAULT + "\n - d (next)"
@@ -9,3 +12,18 @@ alb_mode_string = 'alb'
 perf_search_string = 'se'
 
 chromedriver_update_url = "https://chromedriver.chromium.org"
+
+
+
+"""
+iTunesSearchAPI keys used
+"""
+track_name='trackName'
+artist_name='artistName'
+collection_name='collectionName'
+artworkUrl100='artworkUrl100'
+primary_genre_name='primaryGenreName'
+track_num='trackNumber'
+track_count='trackCount'
+collection_id = 'collectionId'
+collection_artist_name = 'collectionArtistName'
